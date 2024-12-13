13cbdef20de83775c1a4dd0c0ac8275a2c5d2b66
SeaSponge <61109988+Squidwarder@users.noreply.github.com>
2024-12-12 17:24:13 -0800

    rm'd unnecessary files

--- load_yalu_dataset.py
+++
@@ -1,9 +0,0 @@
-from datasets import load_dataset
-from pathlib import Path
-
-# ! use streaming=True to disable downloads
-wildlife_ds = load_dataset("SeaSponge/wildme_dataset", cache_dir=Path.cwd() / "yalu_dataset", streaming=True)
-
-# * This actually works (downside, 300MB dataset shards)
-print("DS from Yalu's modified dataset repo")
-print(wildlife_ds)
